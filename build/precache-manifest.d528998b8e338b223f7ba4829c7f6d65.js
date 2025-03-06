51c0856f2589887ac357a1a8a30d8712e0360879
Keerthi Swaroop <keerthiswaroop@Keerthis-MacBook-Pro-2.local>
2025-03-06 18:42:56 -0500

    added react router directs and Updated homepage field in package.json

---
+++ build/precache-manifest.d528998b8e338b223f7ba4829c7f6d65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f854f47d8901fb2e3665ffc411c0a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce9e8f563b4a7856af63",
+    "url": "/static/css/main.fb4b28ad.chunk.css"
+  },
+  {
+    "revision": "f9abde3280d9184f408d",
+    "url": "/static/js/2.f8309af0.chunk.js"
+  },
+  {
+    "revision": "81c50b5c20c8f5e92a3133aab3083f58",
+    "url": "/static/js/2.f8309af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9e8f563b4a7856af63",
+    "url": "/static/js/main.6a27d3f7.chunk.js"
+  },
+  {
+    "revision": "9782e4b201e91e52ccd4",
+    "url": "/static/js/runtime-main.e4c0243c.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7b6ea1036507c443cca279b39788e5ad.js"
+  "/precache-manifest.d528998b8e338b223f7ba4829c7f6d65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
