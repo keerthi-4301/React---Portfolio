da2946c48abaf05b752bb8162abb062ca19c7b40
Keerthi Swaroop <keerthiswaroop@Keerthis-MacBook-Pro-2.local>
2025-03-06 16:49:51 -0500

    Initial commit

---
+++ build/precache-manifest.99a90290c67f71ef9c895c2d234a56fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe5f5bc6c5f0e9fac52778900a4605c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e79a16d22a1451b52e84",
+    "url": "/portfolio/static/css/main.fb4b28ad.chunk.css"
+  },
+  {
+    "revision": "f9abde3280d9184f408d",
+    "url": "/portfolio/static/js/2.f8309af0.chunk.js"
+  },
+  {
+    "revision": "81c50b5c20c8f5e92a3133aab3083f58",
+    "url": "/portfolio/static/js/2.f8309af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79a16d22a1451b52e84",
+    "url": "/portfolio/static/js/main.f5c08453.chunk.js"
+  },
+  {
+    "revision": "3c8b016ccc6515513155",
+    "url": "/portfolio/static/js/runtime-main.7a06c49e.js"
+  }
+]);
